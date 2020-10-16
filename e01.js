6ee446b29b23dc989c4d865c0dedc9ed4c9fa352
bastien prompsy <bastien.prompsy@efrei.net>
2020-10-16 13:25:14 +0200

    up e01, add e03

--- e01.js
+++ e01.js
@@ -1,3 +1,4 @@
+// function swap pour échanger les nombres
 function swap (str, a, b) {
     let copie = str[a]
     str[a] = str[b]
@@ -5,9 +6,12 @@ function swap (str, a, b) {
 }
 
 function bubble_sort(str) {
+    //boucle pour parcourir le tableau
 for (let i = 0; i < str.length; i +=1) {
+    //si pas bon, envoi dans la fonction swap
     if (str[i] > str[i+1]) {
         swap(str, i, i+1)
+        //i = -1 : pour relancer la boucle au début(pas 0 a cause de l'accrémentation de la boucle)
         i = -1;
     }
 }
